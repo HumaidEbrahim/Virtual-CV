e928d6500ee08b9378b8cd4a5e5b7135ecee09c8
HumaidEbrahim <32646111+HumaidEbrahim@users.noreply.github.com>
2025-07-24 19:26:51 +0200

    gsap animations

--- virtualcv/src/main.js
+++ virtualcv/src/main.js
@@ -1,7 +1,9 @@
 import './style.css'
 import { gsap } from 'gsap'
 import { TextPlugin } from 'gsap/all'
+import ScrollTrigger from "gsap/ScrollTrigger";
 gsap.registerPlugin(TextPlugin)
+gsap.registerPlugin(ScrollTrigger);
 
 gsap.to("h1", {
   duration: 1,
@@ -16,10 +18,61 @@ gsap.from("#about", {
   ease: "power2.out"
 });
 
-gsap.from("#education", {
-  duration: 1.5,
-  x: -50,
+// gsap.from("#education", {
+//   duration: 1.5,
+//   x: -50,
+//   opacity: 0,
+//   ease: "power2.out"
+// });
+
+gsap.from(".timeline-item", {
+  scrollTrigger: {
+    trigger: ".timeline-item",
+    start: "top 80%",
+    toggleActions: "play pause resume reset",
+  },
+  y: 50,
   opacity: 0,
-  ease: "power2.out"
+  duration: 1,
+  ease: "power2.out",
+  stagger: 0.2
 });
 
+gsap.from(".section", {
+  scrollTrigger: {
+    trigger: ".section",
+    start: "top 80%",
+    toggleActions: "play none resume reset",
+  },
+  y: 50,
+  opacity: 0,
+  duration: 1,
+  ease: "power2.out",
+  stagger: 0.2
+});
+
+gsap.from(".edu-section", {
+  scrollTrigger: {
+    trigger: ".edu-section",
+    start: "top 80%",
+    toggleActions: "play none resume reset",
+  },
+  y: 50,
+  opacity: 0,
+  duration: 1,
+  ease: "power2.out",
+  stagger: 0.2
+});
+
+gsap.from(".language", {
+  scale: 0.6,
+  scrollTrigger: {
+    trigger: ".language",
+    start: "top 85%",
+    toggleActions: "play none resume reset"
+  },
+   opacity: 0,
+  duration: 1,
+  ease: "power2.out",
+  stagger: 0.1
+});
